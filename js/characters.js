7a25d963da30fdd73758173213f5b70b785c4fcd
AdrianaFayos <adrifayosperis@gmail.com>
2021-05-11 12:41:38 +0200

    chooseFighter and fillPlayers functions added

--- js/characters.js
+++ js/characters.js
@@ -1,9 +1,10 @@
 class Fighter{
 
-    constructor(name , attack, defense, technique, speed){
+    constructor(name, picName, attack, defense, technique, speed){
         this.life = 300;
         this.luck = Math.floor(Math.random() *11);
         this.name = name;
+        this.picName = picName
         this.attack = attack;
         this.defense = defense;
         this.tecnica = technique;
@@ -21,14 +22,14 @@ class Fighter{
     }
 };
 
-let player1 = new Fighter("Luke Skywalker");
-let player2 = new Fighter("Leia");
-let player3 = new Fighter("Yoda");
-let player4 = new Fighter("Chewbacca");
-let player5 = new Fighter("Darth Vader");
-let player6 = new Fighter("Darth Maul");
-let player7 = new Fighter("Emperor Palpatine");
-let player8 = new Fighter("Stormtrooper");
+let player1 = new Fighter("Luke Skywalker", "lukeSkywalker");
+let player2 = new Fighter("Leia", "leia",);
+let player3 = new Fighter("Yoda", "yoda");
+let player4 = new Fighter("Chewbacca", "chewbacca");
+let player5 = new Fighter("Darth Vader", "darthVader");
+let player6 = new Fighter("Darth Maul", "darthMaul");
+let player7 = new Fighter("Emperor Palpatine", "emperorPalpatine");
+let player8 = new Fighter("Stormtrooper", "stormtrooper");
 
 let allPlayers = {
     "1" : player1,
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+let fighter1 = "";
+let fighter2 = "";
+
 const changeStage = (destination) => {
 
     let arrStage = ["stage1", "stage2", "stage3", "stage4"];
@@ -9,5 +13,55 @@ const changeStage = (destination) => {
     for(let _stage of arrStage){
         document.getElementById(_stage).style.display = "none";
     }
+}
+
+const chooseFighter = (character) => {
+
+    if ( fighter1 == "" ) {
+
+        console.log("entro")
+
+        fighter1 = allPlayers[character];
+        
+        document.getElementById(character).className = "fighterSelected1";
+
+        img1 = document.getElementById("imgFighterSelected1");
+
+        img1.innerHTML = `<img class="imgFighter1" src="img/characters/${fighter1.picName}.png" alt="fighter1" >`;
+
+    } else if ( fighter2 == "" ) {
+
+        fighter2 = allPlayers[character];
+        
+        document.getElementById(character).className = "fighterSelected2";
+
+        img2 = document.getElementById("imgFighterSelected2");
+        img2.innerHTML = `<img class="imgFighter2" src="img/characters/${fighter2.picName}.png" alt="fighter2" >`;
+
+        fillPlayers();
+       
+        setTimeout(() => {
+           changeStage("stage3");
+        }, 2000);
+    }
+
+}
+
+
+
+const fillPlayers = () => {
+
+    let players = document.getElementById("players");
+
+    players.innerHTML = `
+    <div class="fighterOne" >
+         <div class="lifeBar"></div>
+         <div class="picFighter1"><img class="imgFighter1" src="img/characters/${fighter1.picName}.png" alt="fighter1" ></div>
+    </div>
+    <div class="vs">VS</div>
+    <div class="fighterTwo">
+         <div class="lifeBar"></div>
+         <div class="picFighter2"><img class="imgFighter2" src="img/characters/${fighter2.picName}.png" alt="fighter2"></div>
+    </div>`
 }
 
